01c32ae66b599b4589459fdd8d31c5ee97911768
Errors-King <2476987169@qq.com>
2022-10-13 23:50:52 +0800

    feat: add command

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-console.log('hello')
+const { Command  } = require('commander')
+const { helper } = require('./lib/core/helper')
+const { createCommands } = require('./lib/core/create')
+
+
+// 创建处理程序
+const program = new Command()
+
+// -V --version 查看版本号
+program.version(require('./package.json').version)
+
+// 增加 options
+helper(program)
+
+// 增加 commands
+createCommands(program)
+
+// 解析指令
+program.parse(process.argv)
